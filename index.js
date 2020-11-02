48396a82154c2f1fd141fd033087abac4cc720bd
BENMALEKarim <fk_benmalek@esi.dz>
2020-11-02 11:39:50 +0100

    setting up database and server

---
+++ index.js
@@ -0,0 +1,26 @@
+const { ApolloServer } = require('apollo-server');
+const mongoose = require('mongoose');
+
+const { MONGODB } = require('./config');
+const resolvers = require('./graphql/resolvers');
+const typeDefs = require('./graphql/typeDefs');
+
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+
+mongoose
+    .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('MongoDB Connected');
+        return server.listen({ port: 5000 });
+    })
+    .then((res) => {
+        console.log(`Server running at ${res.url}`);
+    })
+    .catch(err => {
+        console.error(err)
+    })
