bbbb24ae59442af294a73d3b8ff3540b0b60d477
Karim Benmalek <32803877+BENMALEKarim@users.noreply.github.com>
2020-11-03 16:22:58 +0100

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    MONGODB: "mongodb+srv://karim:rh4TLBCaneHTqR6g@cluster0.cqrrv.mongodb.net/test?retryWrites=true&w=majority",
-    SECRET_KEY: 'secret key balak wa7ed yedihalek ya kho'
-}
