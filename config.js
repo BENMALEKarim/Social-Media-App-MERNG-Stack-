5aebf052ef6f7bf1597c3b07b7e41fc000493eeb
BENMALEKarim <fk_benmalek@esi.dz>
2020-11-03 15:29:42 +0100

    test

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGODB: "mongodb+srv://karim:rh4TLBCaneHTqR6g@cluster0.cqrrv.mongodb.net/test?retryWrites=true&w=majority",
+    SECRET_KEY: 'secret key balak wa7ed yedihalek ya kho'
+}
